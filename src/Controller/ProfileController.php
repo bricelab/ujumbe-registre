f282158e13417781a3b6603f3d2f5c361237f316
bricelab <bricehessou@gmail.com>
2019-12-05 19:47:41 +0100

    Home and Profile controller added

---
+++ src/Controller/ProfileController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProfileController extends AbstractController
+{
+    /**
+     * @Route("/profile", name="app_profile")
+     */
+    public function index()
+    {
+        return $this->render('profile/index.html.twig', [
+            'controller_name' => 'ProfileController',
+        ]);
+    }
+}
