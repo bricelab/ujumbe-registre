95d3cea0f5df9b4f1d824ca7967fc132f4496fa7
bricelab <bricehessou@gmail.com>
2019-12-05 18:22:05 +0100

    User class modified and Register setup

---
+++ src/Migrations/Version20191205171410.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20191205171410 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE common.app_user ALTER avatar_name DROP NOT NULL');
+        $this->addSql('ALTER TABLE common.app_user ALTER avatar_size DROP NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE common.app_user ALTER avatar_name SET NOT NULL');
+        $this->addSql('ALTER TABLE common.app_user ALTER avatar_size SET NOT NULL');
+    }
+}
