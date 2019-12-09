4833312f70d99fe0c8df054cf86077c66a9646de
bricelab <bricehessou@gmail.com>
2019-12-09 18:59:27 +0100

    Event subscribers class deleted

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,10 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
  /**
@@ -13,10 +17,33 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index()
+    public function index(Request $request)
     {
+        /**
+         * @var FormInterface
+         */
+        $form = $this->createFormBuilder()
+            ->add("locale", ChoiceType::class, [
+                'expanded' => false,
+                'multiple' => false,
+                'choices' => [
+                    'Français' => 'fr',
+                    'English' => 'en',
+                ],
+                'label' => 'layout.locale.changelocal',
+            ])
+            ->add("submit", SubmitType::class, [
+                'label' => 'form.field.locale.submit'
+            ])
+            ->getForm()
+        ;
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            //$form->get;
+        }
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'form' => $form->createView(),
         ]);
     }
 }
